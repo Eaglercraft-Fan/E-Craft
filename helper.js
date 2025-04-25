428fe7f64b3c9626b92a5793b6fc87da639f32c7
Eaglercraft-Fan <Eaglercraft-Fan@outlook.com>
2025-04-25 16:38:17 +0000

    Build Web
    sdf

---
+++ helper.js
@@ -0,0 +1,378 @@
+
+// ================================================================
+
+const EAGLER_1_8_CLIENT = "1.8.8.html";
+const EAGLER_1_8_WASM_CLIENT = "1.8.8-wasm.html";
+const EAGLER_1_12_CLIENT = "1.12.2.html";
+const EAGLER_1_12_WASM_CLIENT = "1.12.2-wasm.html";
+const EAGLER_1_5_CLIENT = "1.5.2.html";
+const EAGLER_B1_3_CLIENT = "b1.3.html";
+const EAGLER_INDEV_CLIENT = "indev.html";
+const EAGLER_A1_2_6_CLIENT = "a1.2.6.html";
+const EAGLER_B1_7_3_CLIENT = "b1.7.3.html";
+const SCRATCH_EDITION_CLIENT = "scratch.html";
+
+
+// START OF CODE ADDED
+const EAGLER_1_8_FD_WEBCLIP = "/dl/EaglercraftX_1.8_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_1_8_IR_WEBCLIP = "/dl/EaglercraftX_1.8_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+const EAGLER_1_5_FD_WEBCLIP = "/dl/Eaglercraft_1.5.2_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_1_5_IR_WEBCLIP = "/dl/Eaglercraft_1.5.2_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+const EAGLER_B1_3_FD_WEBCLIP = "/dl/PeytonPlayz595_b1.7.3_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_B1_3_IR_WEBCLIP = "/dl/PeytonPlayz595_b1.7.3_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+const EAGLER_INDEV_FD_WEBCLIP = "/dl/Eaglercraft_b1.3_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_INDEV_IR_WEBCLIP = "/dl/Eaglercraft_b1.3_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+const EAGLER_A1_2_6_FD_WEBCLIP = "/dl/Eaglercraft_a1.2.6_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_A1_2_6_IR_WEBCLIP = "/dl/Eaglercraft_a1.2.6_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+const EAGLER_B1_7_3_FD_WEBCLIP = "/dl/Eaglercraft_Indev_Webclip_FlamedDoggo99-EaglerMobile.mobileconfig";
+const EAGLER_B1_7_3_IR_WEBCLIP = "/dl/Eaglercraft_Indev_Webclip_irv77-EaglerPocketMobile.mobileconfig";
+// END OF CODE ADDED
+
+const COUNTER = "https://api.api-ninjas.com/v1/counter?id=eaglercraft.com&hit=true";
+
+const EAGLER_1_8_OFFLINE = [ "EaglercraftX_1.8_Offline_Signed_Offline.zip", "EaglercraftX_1.8_Signed_Offline.zip" ];
+const EAGLER_1_8_WASM_OFFLINE = [ "EaglercraftX_1.8_WASM-GC_Offline_Download.zip", "EaglercraftX_1.8_WASM-GC_Offline_Download.zip" ];
+const EAGLER_1_12_OFFLINE = [ "Eaglercraft_1.12_Offline_Download.zip", "Eaglercraft_1.12_Offline_Download.zip" ];
+const EAGLER_1_12_WASM_OFFLINE = [ "Eaglercraft_1.12_WASM_Offline_Download.zip", "Eaglercraft_1.12_WASM_Offline_Download.zip" ];
+const EAGLER_1_5_OFFLINE = [ "Eaglercraft_1.5.1_Offline_Download.zip", "Eaglercraft_1.5.1_Offline_Download.zip" ];
+const EAGLER_B1_3_OFFLINE = [ "Eaglercraft_Beta_1.3_Offline_Download.zip", "Eaglercraft_Beta_1.3_Offline_Download.zip" ];
+const EAGLER_INDEV_OFFLINE = [ "https://bafybeidyxdpjcux6fhwacpn7vbtx6qne4eeudy5onjktdfsnzfbtp3al4e.ipfs.dweb.link/?filename=Indev.zip", "Indev.zip" ];
+const EAGLER_A1_2_6_OFFLINE = [ "Eaglercraft_Alpha_1.2.6_Offline_Download.zip", "Eaglercraft_Alpha_1.2.6_Offline_Download.zip" ];
+const EAGLER_B1_7_3_OFFLINE = [ "Eaglercraft_Beta_1.7.3_Offline_Download.zip", "Eaglercraft_Beta_1.7.3_Offline_Download.zip" ];
+const SCRATCH_EDITION_OFFLINE = [ "Eaglercraft_Scratch_Edition_Offline.zip", "Eaglercraft_Scratch_Edition_Offline.zip" ];
+
+const COUNTER_1_8_OFFLINE = null;
+const COUNTER_1_5_OFFLINE = null;
+const COUNTER_B1_3_OFFLINE = null;
+
+
+const EAGLER_1_8_PLUGIN = [ "https://deev.is/eagler/EaglerXBungee-1.3.6.jar", "EaglerXBungee-1.3.6.jar" ];
+const EAGLER_1_5_PLUGIN = [ "https://deev.is/eagler/eaglerbungee-1.5.2.zip", "eaglerbungee-1.5.2.zip" ];
+const EAGLER_B1_3_PLUGIN = [ "https://deev.is/eagler/eaglerbukkit-b1.3.zip", "eaglerbukkit-b1.3.zip" ];
+
+const COUNTER_1_8_PLUGIN = null;
+const COUNTER_1_5_PLUGIN = null;
+const COUNTER_B1_3_PLUGIN = null;
+
+
+const EAGLER_1_8_SOURCE = "https://git.eaglercraft.rip/eaglercraft/eaglercraft-1.8";
+const EAGLER_1_5_SOURCE = "https://git.eaglercraft.rip/eaglercraft/eaglercraft-1.5";
+const EAGLER_B1_3_SOURCE = "https://git.eaglercraft.rip/eaglercraft/eaglercraft-b1.3";
+const EAGLER_INDEV_SOURCE = "https://git.eaglercraft.rip/3rdparty/peytonplayz585-indev";
+const EAGLER_A1_2_6_SOURCE = "https://git.eaglercraft.rip/3rdparty/peytonplayz585-a1.2.6";
+const EAGLER_B1_7_3_SOURCE = "https://git.eaglercraft.rip/3rdparty/peytonplayz585-b1.7.3";
+const SCRATCH_EDITION_SOURCE = "https://scratch.mit.edu/projects/908626779/";
+
+const COUNTER_1_8_SOURCE = null;
+const COUNTER_1_5_SOURCE = null;
+const COUNTER_B1_3_SOURCE = null;
+const COUNTER_INDEV_SOURCE = null;
+
+
+const EAGLER_1_8_TITLE = "EaglercraftX 1.8.8";
+const EAGLER_1_8_WASM_TITLE = "EaglercraftX 1.8.8 WASM-GC";
+const EAGLER_1_12_TITLE = "Eaglercraft 1.12.2";
+const EAGLER_1_12_WASM_TITLE = "Eaglercraft 1.12.2 WASM-GC";
+const EAGLER_1_5_TITLE = "Eaglercraft 1.5.2";
+const EAGLER_B1_3_TITLE = "Eaglercraft b1.3";
+const EAGLER_INDEV_TITLE = "Indev";
+const EAGLER_A1_2_6_TITLE = "Alpha 1.2.6";
+const EAGLER_B1_7_3_TITLE = "Beta 1.7.3";
+const SCRATCH_EDITION_TITLE = "Scratch Edition";
+
+const IPFS_LOAD_TIME_WARNING = () => alert("Notice: Some links might take several minutes to load!\n\nPlease be patient");
+
+// ================================================================
+
+
+function playNow() {
+    const version = $("#Version option:selected").val();
+    if (version === 'r1.8') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_1_8_CLIENT);
+    }
+    if (version === 'r1.8-wasm') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_1_8_WASM_CLIENT);
+    }
+    if (version === 'r1.12') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_1_12_CLIENT);
+    }
+    if (version === 'r1.12-wasm') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_1_12_WASM_CLIENT);
+    }
+    if (version === 'r1.5') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_1_5_CLIENT);
+    }
+    if (version === 'b1.3') {
+        if(typeof COUNTER === "string") $.ajax({method: 'GET', headers: { 'X-Api-Key': 'L+V7cvfWHQOvVDYfyYjkiA==9vs8Sk4DrXIOpNSZ'}, url: COUNTER, dataType: "script", success: ""});
+        location.href = addUserscriptQuery(EAGLER_B1_3_CLIENT);
+    }
+    if (version === 'indev') {
+        location.href = addUserscriptQuery(EAGLER_INDEV_CLIENT);
+    }
+    if (version === 'a1.2.6') {
+        location.href = addUserscriptQuery(EAGLER_A1_2_6_CLIENT);
+    }
+    if (version === 'b1.7.3') {
+        location.href = addUserscriptQuery(EAGLER_B1_7_3_CLIENT);
+    }
+    if (version === 'scratch') {
+        location.href = addUserscriptQuery(SCRATCH_EDITION_CLIENT);
+    }
+}
+
+// START OF CODE ADDED
+
+function webClip() {
+    if (version === 'r1.8') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_1_8_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_1_8_FD_WEBCLIP;
+        }
+    }
+    if (version === 'r1.8-wasm') {
+        alert("Coming soon!");
+    }
+    if (version === 'r1.12') {
+        alert("Coming soon!");
+    }
+    if (version === 'r1.12-wasm') {
+        alert("Coming soon!");
+    }
+    if (version === 'r1.5') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_1_5_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_1_5_FD_WEBCLIP;
+        }
+    }
+    if (version === 'b1.3') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_B1_3_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_B1_3_FD_WEBCLIP;
+        }
+    }
+    if (version === 'indev') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_INDEV_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_INDEV_FD_WEBCLIP;
+        }
+    }
+    if (version === 'a1.2.6') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_A1_2_6_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_A1_2_6_FD_WEBCLIP;
+        }
+    }
+    if (version === 'b1.7.3') {
+        if($("#user-script-combobox.dropdown").val() === "irv77-eaglercraft-mobile.js") {
+            location.href = EAGLER_B1_7_3_IR_WEBCLIP;
+        } else {
+            location.href = EAGLER_B1_7_3_FD_WEBCLIP;
+        }
+    }
+    if (version === 'scratch') {
+        alert("This version is not available for iOS.")
+    }
+}
+
+// END OF CODE ADDED
+
+function downloadFileFromURL(urlIn, filenameOut) {
+    const cunt = document.createElement("a");
+    cunt.href = urlIn;
+    cunt.download = filenameOut;
+    cunt.click();
+}
+
+function downloadOfflineNow() {
+    const version = $("#Version option:selected").val();
+    if (version === 'r1.8') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+       downloadFileFromURL(EAGLER_1_8_OFFLINE[0], EAGLER_1_8_OFFLINE[1]);
+    }
+    if (version === 'r1.8-wasm') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+       downloadFileFromURL(EAGLER_1_8_WASM_OFFLINE[0], EAGLER_1_8_WASM_OFFLINE[1]);
+    }
+    if (version === 'r1.12') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+       downloadFileFromURL(EAGLER_1_12_OFFLINE[0], EAGLER_1_12_OFFLINE[1]);
+    }
+    if (version === 'r1.12-wasm') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+       downloadFileFromURL(EAGLER_1_12_WASM_OFFLINE[0], EAGLER_1_12_WASM_OFFLINE[1]);
+    }
+    if (version === 'r1.5') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+        downloadFileFromURL(EAGLER_1_5_OFFLINE[0], EAGLER_1_5_OFFLINE[1]);
+    }
+    if (version === 'b1.3') {
+        IPFS_LOAD_TIME_WARNING();
+        if(typeof COUNTER === "string") $.ajax({url: COUNTER, dataType: "script", success: ""});
+        downloadFileFromURL(EAGLER_B1_3_OFFLINE[0], EAGLER_B1_3_OFFLINE[1]);
+    }
+    if (version === 'indev') {
+        downloadFileFromURL(EAGLER_INDEV_OFFLINE[0], EAGLER_INDEV_OFFLINE[1]);
+    }
+    if (version === 'a1.2.6') {
+        downloadFileFromURL(EAGLER_A1_2_6_OFFLINE[0], EAGLER_A1_2_6_OFFLINE[1]);
+    }
+    if (version === 'b1.7.3') {
+        downloadFileFromURL(EAGLER_B1_7_3_OFFLINE[0], EAGLER_B1_7_3_OFFLINE[1]);
+    }
+    if (version === 'scratch') {
+        downloadFileFromURL(SCRATCH_EDITION_OFFLINE[0], SCRATCH_EDITION_OFFLINE[1]);
+    }
+}
+
+function downloadPluginNow() {
+    const version = $("#Version option:selected").val();
+    if (version === 'r1.8') {
+        downloadFileFromURL(EAGLER_1_8_PLUGIN[0], EAGLER_1_8_PLUGIN[1]);
+    }
+    if (version === 'r1.8-wasm') {
+        downloadFileFromURL(EAGLER_1_8_PLUGIN[0], EAGLER_1_8_PLUGIN[1]);
+    }
+    if (version === 'r1.12') {
+        downloadFileFromURL(EAGLER_1_8_PLUGIN[0], EAGLER_1_8_PLUGIN[1]);
+    }
+    if (version === 'r1.12-wasm') {
+        downloadFileFromURL(EAGLER_1_8_PLUGIN[0], EAGLER_1_8_PLUGIN[1]);
+    }
+    if (version === 'r1.5') {
+        downloadFileFromURL(EAGLER_1_5_PLUGIN[0], EAGLER_1_5_PLUGIN[1]);
+    }
+    if (version === 'b1.3') {
+        downloadFileFromURL(EAGLER_B1_3_PLUGIN[0], EAGLER_B1_3_PLUGIN[1]);
+    }
+}
+
+function downloadSourceNow() {
+    const version = $("#Version option:selected").val();
+    if (version === 'r1.8') {
+        location.href = EAGLER_1_8_SOURCE;
+    }
+    if (version === 'r1.8-wasm') {
+        location.href = EAGLER_1_8_SOURCE;
+    }
+    if (version === 'r1.12') {
+        alert("Coming Soon!");
+    }
+    if (version === 'r1.12-wasm') {
+        alert("Coming Soon!");
+    }
+    if (version === 'r1.5') {
+        location.href = EAGLER_1_5_SOURCE;
+    }
+    if (version === 'b1.3') {
+        location.href = EAGLER_B1_3_SOURCE;
+    }
+    if (version === 'indev') {
+        location.href = EAGLER_INDEV_SOURCE;
+    }
+    if (version === 'a1.2.6') {
+        location.href = EAGLER_A1_2_6_SOURCE;
+    }
+    if (version === 'b1.7.3') {
+        location.href = EAGLER_B1_7_3_SOURCE;
+    }
+    if (version === 'scratch') {
+        location.href = SCRATCH_EDITION_SOURCE;
+    }
+}
+
+function updateDownloadLinkText() {
+    const version = $("#Version option:selected").val();
+    if (version === 'r1.8') {
+        $("#download-title").text("Get " + EAGLER_1_8_TITLE);
+    }
+    if (version === 'r1.8-wasm') {
+        $("#download-title").text("Get " + EAGLER_1_8_WASM_TITLE);
+    }
+    if (version === 'r1.12') {
+        $("#download-title").text("Get " + EAGLER_1_12_TITLE);
+    }
+    if (version === 'r1.12-wasm') {
+        $("#download-title").text("Get " + EAGLER_1_12_WASM_TITLE);
+    }
+    if (version === 'r1.5') {
+        $("#download-title").text("Get " + EAGLER_1_5_TITLE);
+    }
+    if (version === 'b1.3') {
+        $("#download-title").text("Get " + EAGLER_B1_3_TITLE);
+    }
+    if (version === 'indev') {
+        $("#download-title").text("Get " + EAGLER_INDEV_TITLE);
+    }
+    if (version === 'a1.2.6') {
+        $("#download-title").text("Get " + EAGLER_A1_2_6_TITLE);
+    }
+    if (version === 'b1.7.3') {
+        $("#download-title").text("Get " + EAGLER_B1_7_3_TITLE);
+    }
+    if (version === 'scratch') {
+        $("#download-title").text("Get " + SCRATCH_EDITION_TITLE);
+    }
+}
+
+function checkNotMobileBrowser() {
+    try {
+        document.exitPointerLock();
+        return !(/Mobi/i.test(window.navigator.userAgent));
+    }catch(e) {
+        return false;
+    }
+}
+
+function updateTouchModeChecked() {
+    if($("#touch-mode-checkbox").is(":checked")) {
+        $("#user-script-combobox").show();
+    }else {
+        $("#user-script-combobox").hide();
+    }
+}
+
+function addUserscriptQuery(url) {
+    if($("#touch-mode-checkbox").is(":checked")) {
+        return url + "?userscript=" + $("#user-script-combobox option:selected").val();
+    }else {
+        return url;
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    $("#play-now").on("click", playNow);
+    $("#download-webclip").on("click", webClip);
+    $("#download-offline").on("click", downloadOfflineNow);
+    $("#download-plugin").on("click", downloadPluginNow);
+    $("#download-source").on("click", downloadSourceNow);
+    $("#Version").on("change", updateDownloadLinkText);
+    $("#touch-mode-checkbox").on("change", updateTouchModeChecked);
+    if(!checkNotMobileBrowser()) {
+        $("#touch-mode-checkbox").prop("checked", true);
+    }
+    updateTouchModeChecked();
+});
+
+window.addEventListener("load", () => {
+    window.setTimeout(() => {
+        if(!checkNotMobileBrowser()) {
+            $("#touch-mode-checkbox").prop("checked", true);
+        }
+        updateTouchModeChecked();
+    }, 100);
+});
